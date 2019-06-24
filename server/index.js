b2aa637a97a0a6e938dadce2bc796915c3029da3
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-06-24 11:52:07 -0600

    set up basic express server and modified package.json

---
+++ server/index.js
@@ -0,0 +1,10 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const {SERVER_PORT} = process.env;
+
+app.use(express.json());
+
+app.listen(SERVER_PORT, () => {
+    console.log(`Server is running on port ${SERVER_PORT}`);
+});
