2d93c2c9b012ea665654c0687ffd0853331bbaac
Tyler Ellingford <tyler@Rickys-MacBook-Pro.local>
2019-06-24 12:11:16 -0600

    set up database connection, fixed small bugs

--- server/index.js
+++ server/index.js
@@ -1,10 +1,15 @@
 require('dotenv').config();
-const express = require('express');
+const express = require('express'),
+      massive = require('massive');
 const app = express();
-const {SERVER_PORT} = process.env;
+const {SERVER_PORT, CONNECTION_STRING} = process.env;
 
 app.use(express.json());
 
-app.listen(SERVER_PORT, () => {
-    console.log(`Server is running on port ${SERVER_PORT}`);
+massive(CONNECTION_STRING).then((database) => {
+    app.set('db', database);
+    console.log('Database is connected!');
+    app.listen(SERVER_PORT, () => {
+        console.log(`Server is running on port ${SERVER_PORT}`);
+    });
 });
